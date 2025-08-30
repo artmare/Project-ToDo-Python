77b123c4db55ac907a4e3b78859fb68f002e5c9c
artmare <122673491+artmare@users.noreply.github.com>
2025-08-30 16:08:18 +0200

    main file

---
+++ main.py
@@ -0,0 +1,43 @@
+# 1. [ ] Купить молоко
+# 2. [ ] Сделать домашку
+# 3. [x] Пробежка утром
+
+
+tasks = {}
+
+print("Команды: add, list, done, delete, exit")
+
+while True:
+    comm = input('write a command ')
+    
+    if comm == 'add':
+        task = input('write a task ')
+        tasks[task] = False
+        print(f'task "{task}" was added ')
+    elif comm == 'list':
+        if not tasks:
+            print('Your list is empty ')
+        else:
+            for i, (task, done) in enumerate(tasks.items(), start=1):
+                status = '[x]' if done else '[ ]'
+                print(f'{i}. {status} {task}')
+    elif comm == 'done':
+        task = input('write a task ')
+        if task not in tasks:
+            print('Here is not your task!')
+        else:
+            tasks[task] = True
+            print(f'Task {task} is done ')
+        
+    elif comm == 'delete':
+        task = input('write a task ')
+        if task not in tasks:
+            print('Here is not your task!')
+        else:
+            del tasks[task]
+            print(f'Task {task} was deleted')
+    elif comm == 'exit':
+        print('Exiting ')
+        break
+    else:
+        print('I do not understand you ')
